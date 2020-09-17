30bfdb5b1efce5062e2b8a1ba58496d3f95535eb
Davíð Örn Harðarson <davidh20@ru.is>
2020-09-17 11:16:11 +0000

    Davíð1

--- Tile_traveller.py
+++ Tile_traveller.py
@@ -32,7 +32,7 @@ def possible_direction(x,y):
     if y == 1:
         direction_str = "N"
     if x == 1 and y == 2:
-        direction_str = "N or E or S"
+        direction_str = "NES"
     if x == 1 and y == 3:
         direction_str = "E or S"
     if x == y:
@@ -46,12 +46,34 @@ def possible_direction(x,y):
 def check_victory():
 
 victory = False
+valid_direction = False
 x_cordinates = 1
 y_cordinates = 1
 
 
 while victory = False:
-    print()
+    print('You can travel: 'end=)
+    possible_direction = possible_direction()
+    
+    while valid_direction == True:
+        new_move = input("Direction: ")
+        new_move = new_move.upper()
+        for letter in possible_direction:
+            if letter == new_move:
+                valid_direction = True
+            else:
+                print('Not a valid direction!')
+
+    x = move_player_x(x, new_move)
+    y = move_player_y(y, new_move)
+
+    if x == 3 and y == 3:
+        print('Victory!')
+        break
+
+        
+
+
 
 # 1.1 (N)orth
 # 1.2 (N)orth or (E)ast or (S)outh
