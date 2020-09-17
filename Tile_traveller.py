3213597f212a8c22c6785aca739360f8fe1b7fba
Telma Guðmundsdóttir <telmagudmundsdottir99@gmail.com>
2020-09-17 10:26:43 +0000

    directions

--- Tile_traveller.py
+++ Tile_traveller.py
@@ -15,3 +15,12 @@ victory = False
 while victory = False:
     pass
 
+# 1.1 (N)orth
+# 1.2 (N)orth or (E)ast or (S)outh
+# 1.3 (E)ast or (S)outh
+# 2.1 (N)orth
+# 2.2 (S)outh or (W)est
+# 2.3 (E)ast or (W)est
+# 3.1 (N)orth
+# 3.2 (N)orth or (S)outh
+# 3.3 (S)outh or (W)est
