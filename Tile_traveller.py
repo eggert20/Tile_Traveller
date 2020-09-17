9ded81f04196f57fdc47df39a6cf360f08fdbb27
Davíð Örn Harðarson <davidh20@ru.is>
2020-09-17 10:20:48 +0000

    Beinagrind

--- Tile_traveller.py
+++ Tile_traveller.py
@@ -2,4 +2,16 @@
 welcome = "Hello world"
 print(welcome)
 
+def change_location():
+
+def locate_player():
+
+def possible_direction():
+
+def check_victory():
+
+victory = False
+
+while victory = False:
+    pass
 
